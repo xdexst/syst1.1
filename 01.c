85359e58ea7252e0de7e06baae7d4346d0929d87
xdexst <rnoy10303@gmail.com>
2023-06-10 11:51:23 +0400

    practice1

---
+++ 01.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main () {
+int birthYear = 2001;
+int nowYear = 2010;
+int ageInThisYear ;
+ageInThisYear = nowYear - birthYear;
+printf("Возраст в %d году:%d лет.\n", nowYear, ageInThisYear);
+return 0;
+}
